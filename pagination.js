7481d9f63a393ef66251c3f5d458c260becb39a5
Nguyen.Minh.Duc <nguytest039@gmail.com>
2026-01-26 14:39:10 +0700

    Remove FrontendPagination class and related code
    Removed the FrontendPagination class and its related code, simplifying the pagination library to focus on backend functionality.

--- pagination.js
+++ pagination.js
@@ -1,14 +1,14 @@
 /*!
  * Pagination library
- * Author: DKN(DUC) 
+ * Author: DKN(DUC)
  * © 2026
  */
 
 /**
  * Backend Pagination Library
- * 
+ *
  * import { Pagination } from './pagination.js';
- * 
+ *
  * const pagination = new Pagination({
  *     container: '#pagination',
  *     totalItems: 100,
@@ -55,8 +55,8 @@ export class Pagination {
     }
 
     init() {
-        this.container = typeof this.options.container === 'string' 
-            ? document.querySelector(this.options.container) 
+        this.container = typeof this.options.container === 'string'
+            ? document.querySelector(this.options.container)
             : this.options.container;
 
         if (!this.container) {
@@ -83,7 +83,7 @@ export class Pagination {
         const total = this.totalPages;
         const current = this.currentPage;
         const max = this.options.maxVisiblePages;
-        
+
         if (total <= max) return Array.from({ length: total }, (_, i) => i + 1);
 
         const pages = [];
@@ -111,7 +111,7 @@ export class Pagination {
     getInfoText() {
         const start = (this.currentPage - 1) * this.options.itemsPerPage + 1;
         const end = Math.min(this.currentPage * this.options.itemsPerPage, this.options.totalItems);
-        
+
         return this.options.labels.info
             .replace('{start}', start)
             .replace('{end}', end)
@@ -251,7 +251,7 @@ export class Pagination {
         btn.className = 'pagination-btn';
         btn.textContent = text;
         btn.title = title;
-        
+
         if (!this.options.disabled) {
             btn.addEventListener('click', e => {
                 e.preventDefault();
@@ -338,169 +338,4 @@ export class Pagination {
     }
 }
 
-/**
- * Frontend Pagination - Client-side
- * 
- * import { FrontendPagination } from './pagination.js';
- * 
- * const frontPagination = new FrontendPagination({
- *     container: '#pagination',
- *     data: myDataArray,
- *     itemsPerPage: 10,
- *     renderItems: (items) => {
- *         const list = document.getElementById('my-list');
- *         list.innerHTML = items.map(item => `<div>${item.name}</div>`).join('');
- *     }
- * });
- */
-
-export class FrontendPagination extends Pagination {
-    constructor(options = {}) {
-        const data = options.data || [];
-        
-        super({
-            ...options,
-            totalItems: data.length,
-            onPageChange: null
-        });
-
-        this.data = data;
-        this.renderItems = options.renderItems || null;
-        this.onPageChangeCallback = options.onPageChange || null;
-
-        this.renderCurrentPage();
-    }
-
-    getCurrentPageData() {
-        const start = (this.currentPage - 1) * this.options.itemsPerPage;
-        const end = start + this.options.itemsPerPage;
-        return this.data.slice(start, end);
-    }
-
-    renderCurrentPage() {
-        const pageData = this.getCurrentPageData();
-        
-        if (typeof this.renderItems === 'function') {
-            this.renderItems(pageData, {
-                currentPage: this.currentPage,
-                totalPages: this.totalPages,
-                totalItems: this.data.length,
-                startIndex: (this.currentPage - 1) * this.options.itemsPerPage,
-                endIndex: Math.min(this.currentPage * this.options.itemsPerPage, this.data.length)
-            });
-        }
-    }
-
-    async goToPage(page) {
-        if (page < 1 || page > this.totalPages || page === this.currentPage) return;
-
-        this.currentPage = page;
-        this.render();
-        this.renderCurrentPage();
-
-        if (typeof this.onPageChangeCallback === 'function') {
-            try {
-                await this.onPageChangeCallback(this.getCurrentPageData(), {
-                    currentPage: this.currentPage,
-                    totalPages: this.totalPages,
-                    itemsPerPage: this.options.itemsPerPage
-                });
-            } catch (error) {
-                console.error('FrontendPagination: Error in onPageChange callback', error);
-            }
-        }
-    }
-
-    async setItemsPerPage(itemsPerPage) {
-        this.options.itemsPerPage = itemsPerPage;
-        this.currentPage = 1;
-        this.render();
-        this.renderCurrentPage();
-
-        if (typeof this.onPageChangeCallback === 'function') {
-            try {
-                await this.onPageChangeCallback(this.getCurrentPageData(), {
-                    currentPage: this.currentPage,
-                    totalPages: this.totalPages,
-                    itemsPerPage: this.options.itemsPerPage
-                });
-            } catch (error) {
-                console.error('FrontendPagination: Error in onPageChange callback', error);
-            }
-        }
-    }
-
-    setData(data, resetPage = true) {
-        this.data = data || [];
-        this.options.totalItems = this.data.length;
-        
-        if (resetPage) {
-            this.currentPage = 1;
-        } else if (this.currentPage > this.totalPages) {
-            this.currentPage = this.totalPages || 1;
-        }
-        
-        this.render();
-        this.renderCurrentPage();
-    }
-
-    addItem(item) {
-        this.data.push(item);
-        this.options.totalItems = this.data.length;
-        this.render();
-    }
-
-    addItems(items) {
-        this.data.push(...items);
-        this.options.totalItems = this.data.length;
-        this.render();
-    }
-
-    removeItemAt(index) {
-        if (index >= 0 && index < this.data.length) {
-            this.data.splice(index, 1);
-            this.options.totalItems = this.data.length;
-            
-            if (this.currentPage > this.totalPages && this.totalPages > 0) {
-                this.currentPage = this.totalPages;
-            }
-            
-            this.render();
-            this.renderCurrentPage();
-        }
-    }
-
-    removeItemWhere(predicate) {
-        const index = this.data.findIndex(predicate);
-        if (index !== -1) this.removeItemAt(index);
-    }
-
-    filter(predicate) {
-        const filteredData = this.data.filter(predicate);
-        return new FrontendPagination({
-            ...this.options,
-            data: filteredData,
-            renderItems: this.renderItems,
-            onPageChange: this.onPageChangeCallback
-        });
-    }
-
-    search(predicate) {
-        const filteredData = this.data.filter(predicate);
-        this.setData(filteredData);
-    }
-
-    resetSearch(originalData) {
-        this.setData(originalData);
-    }
-
-    sort(compareFn) {
-        this.data.sort(compareFn);
-        this.renderCurrentPage();
-    }
-
-    getData() { return this.data; }
-    getDataLength() { return this.data.length; }
-}
-
-export default { Pagination, FrontendPagination };
+export default Pagination;
